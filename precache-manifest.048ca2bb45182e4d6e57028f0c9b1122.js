b48642df3763ef026300f6e58597f6b4f6724b0a
ainurmurmur <sagandykovaainur@gmail.com>
2020-07-01 19:23:40 +0300

    Updates

---
+++ precache-manifest.048ca2bb45182e4d6e57028f0c9b1122.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf4a2df68766c9f2a56eeb8a0346b52a",
+    "url": "/ainurmurmur/to-do-list.git/index.html"
+  },
+  {
+    "revision": "3711254961580041cae4",
+    "url": "/ainurmurmur/to-do-list.git/static/css/main.6f01a9bf.chunk.css"
+  },
+  {
+    "revision": "290332e35ea57793b971",
+    "url": "/ainurmurmur/to-do-list.git/static/js/2.199a0260.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ainurmurmur/to-do-list.git/static/js/2.199a0260.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3711254961580041cae4",
+    "url": "/ainurmurmur/to-do-list.git/static/js/main.17239bff.chunk.js"
+  },
+  {
+    "revision": "5afd0351247ffa89cb53",
+    "url": "/ainurmurmur/to-do-list.git/static/js/runtime-main.8474f947.js"
+  }
+]);
